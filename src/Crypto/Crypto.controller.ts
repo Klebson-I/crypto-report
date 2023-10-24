ee677408877b64120b91e2c5ebd6b36d9dbffff4
Łukasz Klęba <lukasz.kleba99@onet.pl>
2023-10-24 17:32:10 +0200

    update eslint rule, create controller and service for /crypto

---
+++ src/Crypto/Crypto.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Post } from '@nestjs/common';
+import { CryptoService } from './Crypto.service';
+
+@Controller('crypto')
+export class CryptoController {
+  constructor(private cryptoService: CryptoService) {}
+
+  @Post('/createData')
+  async createReportsData() {}
+}
