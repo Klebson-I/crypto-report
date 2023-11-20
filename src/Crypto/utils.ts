2b37455ed5d3c25a5c8774bdd58066321f6eef2e
Łukasz Klęba <lukasz.kleba99@onet.pl>
2023-11-20 09:48:51 +0100

    create utils function to return avg, max, min prices for crypto from reports

--- src/Crypto/utils.ts
+++ src/Crypto/utils.ts
@@ -11,6 +11,50 @@ export const getCurrencyData = async (currencies: GetCurrencyDataInput) => {
   return data;
 };
 
+const getRates = (a) => a.map(({ rate }) => rate);
+
+const getAverageValue = (reports: JoinCurrencyDto[]): number => {
+  const prices = getRates(reports);
+  const sum = prices.reduce((acc, curr) => (acc += curr), 0);
+  const avg = sum / prices.length;
+  return avg;
+};
+
+const getMaxValue = (reports: JoinCurrencyDto[]): number => {
+  const prices = getRates(reports);
+  const max = Math.max(...prices);
+  return max;
+};
+
+const getMinValue = (reports: JoinCurrencyDto[]): number => {
+  const prices = getRates(reports);
+  const min = Math.min(...prices);
+  return min;
+};
+
 export const getCurrenciesStatisticInformation = (data: JoinCurrencyDto[]) => {
-  console.log(data);
+  const specificCurrenciesInformation = data.reduce((acc, curr, _, arr) => {
+    const { asset_id_base: actualIdBase } = curr;
+
+    if (acc.hasOwnProperty(actualIdBase)) {
+      return acc;
+    }
+
+    const allReportsInType = arr.filter(
+      ({ asset_id_base }) => asset_id_base === actualIdBase,
+    );
+
+    const avgValue = getAverageValue(allReportsInType);
+    const maxValue = getMaxValue(allReportsInType);
+    const minValue = getMinValue(allReportsInType);
+
+    acc[actualIdBase] = {
+      averagePrive: avgValue,
+      maxPrice: maxValue,
+      minPrice: minValue,
+    };
+
+    return acc;
+  }, {});
+  return specificCurrenciesInformation;
 };
